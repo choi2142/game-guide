1bdbe864b40fb4b4ea09076a37764e22249e7bd2
akceai <akceai@akc.co.kr>
2025-03-26 17:05:00 +0900

    @typescript-eslint/no-explicit-any 규칙 개선

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
-// pages/_app.tsx
-
+import { AppProps } from 'next/app'; // AppProps 타입을 가져옵니다.
 import RootLayout from "../layouts/RootLayout"; // RootLayout 임포트
 import "../styles/global.css"; // 글로벌 CSS 스타일 임포트
 
-function MyApp({ Component, pageProps }: { Component: React.ElementType, pageProps: any }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RootLayout>
       <Component {...pageProps} /> {/* Component는 index.tsx에 해당하는 페이지 컴포넌트를 의미 */}
